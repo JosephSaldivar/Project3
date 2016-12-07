a565c6d0777190bceb27b9d5c9257d805ffdd0b6
Scott Rigg <srigg@hawk.iit.edu>
2016-12-06 21:18:12 -0600

    Added javascript for displaying abilities

--- site.js
+++ site.js
@@ -11,7 +11,6 @@ $.noConflict();
           function(data,json) {
 
             if (data != null) {
-<<<<<<< HEAD
            var name = data.name;
            //var ability = data.abilities[0];
            var evo = data.evolutions[0].to;
@@ -19,39 +18,34 @@ $.noConflict();
            //var type1 = data.types[0].name;
            //var type2 = data.types[1].name;
            var types = "";
-           /*for (var i = 0; i < data.types.length; i++)
-           {
-             var type_to_add = (data.types[i].name);
-             type_to_add=type_to_add.charAt(0).toUpperCase()+type_to_add.slice(1, (type_to_add.length));
-             types+= type_to_add + " ";
-           }*/
-           var abilities = data.abilities.join(", ");
-=======
-           var pokename = data.name;
-           var ability = data.abilities[0];
-           var pokeevo = data.evolutions[0].to;
-           var pokesprite = data.sprites[0].name;
-           //var type1 = data.types[0].name;
-           //var type2 = data.types[1].name;
-           var poketype = "";
            for (var i = 0; i < data.types.length; i++)
            {
              var type_to_add = (data.types[i].name);
              type_to_add=type_to_add.charAt(0).toUpperCase()+type_to_add.slice(1, (type_to_add.length));
-             poketype+= type_to_add + "";
+             types+= type_to_add + " ";
            }
->>>>>>> 708ed04000180d90fb374cb6c4b5357952ad3fb7
+           var abilities =""+ data.abilities.join(", ");
+           //var types = ""+data.types.join(", ");
+           //var abilities = "";
+           
+           
  };
 
+ var pokename = name;
  $('#pokename').text(pokename);
+
+ var pokeevo = evo;
  $('#pokeevo').text(pokeevo);
 
  //var poketype1 = type1;
  //var poketype2 = type2;
  //var poketype = type1 + ' and ' + type2;
+ var poketype = types;
  $('#poketype').text(poketype);
 
-
+ var pokeability = abilities;
+ $('#pokeability').text(pokeability);
+ 
   var url = 'http://pokeapi.co/api/v1/sprite/' + (num + 1);
   $.get(url).done(
   function(data,json) {
@@ -60,13 +54,8 @@ $.noConflict();
       var sprite = data.image;
 
   };
-<<<<<<< HEAD
   
   var pokesprite = sprite;
-=======
-
-
->>>>>>> 708ed04000180d90fb374cb6c4b5357952ad3fb7
   $('#pokesprite').text(pokesprite);
 
 
