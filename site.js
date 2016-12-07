09dc336b23dc0e84d2cf6917a8c2b129f8d92151
stephen-grzenia <pikathechu7@gmail.com>
2016-12-06 19:57:45 -0600

    updating javascript and css

--- site.js
+++ site.js
@@ -11,31 +11,27 @@ $.noConflict();
           function(data,json) {
 
             if (data != null) {
-           var name = data.name;
+           var pokename = data.name;
            var ability = data.abilities[0];
-           var evo = data.evolutions[0].to;
-           var sprite = data.sprites[0].name;
+           var pokeevo = data.evolutions[0].to;
+           var pokesprite = data.sprites[0].name;
            //var type1 = data.types[0].name;
            //var type2 = data.types[1].name;
-           var types = "";
+           var poketype = "";
            for (var i = 0; i < data.types.length; i++)
            {
              var type_to_add = (data.types[i].name);
              type_to_add=type_to_add.charAt(0).toUpperCase()+type_to_add.slice(1, (type_to_add.length));
-             types+= type_to_add + "";
+             poketype+= type_to_add + "";
            }
  };
 
- var pokename = name;
  $('#pokename').text(pokename);
-
- var pokeevo = evo;
  $('#pokeevo').text(pokeevo);
 
  //var poketype1 = type1;
  //var poketype2 = type2;
  //var poketype = type1 + ' and ' + type2;
- var poketype = types;
  $('#poketype').text(poketype);
 
 
@@ -48,7 +44,7 @@ $.noConflict();
 
   };
 
-  var pokesprite = sprite;
+  
   $('#pokesprite').text(pokesprite);
 
 
